da089660f32f6155bdfe3b595aa10145f54ca378
Johannes Schickling <schickling.j@gmail.com>
2016-03-09 11:33:01 +0100

    playground endpoint

--- src/views/PlaygroundView/PlaygroundView.js
+++ src/views/PlaygroundView/PlaygroundView.js
@@ -4,6 +4,10 @@ import classes from './PlaygroundView.scss'
 
 import 'graphiql/graphiql.css'
 
+const api = process.env.NODE_ENV === 'production'
+  ? 'http://api.alpha.graph.cool:60000/graphql'
+  : 'http://localhost:60000/graphql'
+
 export default class PlaygroundView extends React.Component {
   static propTypes = {
     params: PropTypes.object.isRequired,
@@ -12,7 +16,7 @@ export default class PlaygroundView extends React.Component {
   render () {
     const token = window.localStorage.getItem('token')
     const fetcher = (graphQLParams) => (
-      fetch(`http://localhost:60000/graphql/${this.props.params.projectId}`, {
+      fetch(`${api}/${this.props.params.projectId}`, {
         method: 'post',
         headers: {
           'Content-Type': 'application/json',
