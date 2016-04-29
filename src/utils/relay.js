295684de2ff9e1b8bc36b518e16c7c88313731f0
Johannes Schickling <schickling.j@gmail.com>
2016-04-29 16:44:33 +0200

    change env to DASHBOARD_SCHEMA_ENDPOINT

--- src/utils/relay.js
+++ src/utils/relay.js
@@ -2,7 +2,10 @@ import { injectNetworkLayer, DefaultNetworkLayer } from 'react-relay'
 
 export function updateNetworkLayer () {
   const token = window.localStorage.getItem('token')
-  const headers = token ? { Authorization: `Bearer ${token}`, 'X-GraphCool-Source': 'dashboard:relay' } : null
+  const headers = token ? {
+    'Authorization': `Bearer ${token}`,
+    'X-GraphCool-Source': 'dashboard:relay',
+  } : null
   const api = `${__BACKEND_ADDR__}/api`
   const layer = new DefaultNetworkLayer(api, { headers, retryDelays: [] })
 
